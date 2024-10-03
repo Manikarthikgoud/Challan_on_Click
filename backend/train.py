3b94c866f261e05dc9078e5831118c461976fe1d
Muthagari Manikarthik Goud <40057627+Manikarthikgoud@users.noreply.github.com>
2024-10-03 12:14:08 +0530

    Delete backend directory

--- backend/train.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO("yolov8m.yaml")  # build a new model from scratch
-
-# Use the model
-model.train(data="dataset.yaml", epochs=100)  # train the model
